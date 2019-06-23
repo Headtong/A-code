4f341e58f0381743648d839fafc090587b670e43
apple <apple@appledeMacBook-Air.local>
2019-06-23 20:25:10 +0800

    上传代码

---
+++ Desktop/沈工微搜小助手/SITminiprogram/pages/index/aboutUs/aboutUs.js
@@ -0,0 +1,66 @@
+// miniprogram/pages/index/aboutUs/aboutUs.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    index: '<h1 class="div_class" style="line-height: 60px;"><strong>关于小程序</strong></h1></br> <p class="P_class" style="padding:12px;border:solid #ddd 2px">沈工微搜小程序是由沈阳工学院信息大创团队沈工小助手开发团队的共同开发成果,最终版权归沈工小助手开发团队所有。感谢所有测试人员与协助开发人员的支持与奉献。</p>'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
